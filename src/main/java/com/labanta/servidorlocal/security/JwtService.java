6519e400474dc49ab1fd3adcb608f7b2bb252144
Djeison Santos <djeison.adventista@gmail.com>
2026-08-28 14:09:16 -0100

    Dockerfile

--- src/main/java/com/labanta/servidorlocal/security/JwtService.java
+++ src/main/java/com/labanta/servidorlocal/security/JwtService.java
@@ -10,11 +10,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.util.Date;
 
-@Service
-public class JwtService {
-    private final SecretKey chaveSecreta = Keys.hmacShaKeyFor(
-            "${JWT_SECRET}".getBytes()
-    );
+@Service public class JwtService { @Value("${jwt.secret}") private String secret; private SecretKey getChaveSecreta() { byte[] keyBytes = Decoders.BASE64.decode(secret); return Keys.hmacShaKeyFor(keyBytes); }
 
     public String gerarToken(String username) {
 
@@ -22,14 +18,14 @@ public class JwtService {
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 3600000)) //1 hora
-                .signWith(chaveSecreta)
+                .signWith(getChaveSecreta())
                 .compact();
     }
 
     public String extrairUsername(String token) {
 
         return Jwts.parser()
-                .verifyWith(chaveSecreta)
+                .verifyWith(getChaveSecreta())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload()
