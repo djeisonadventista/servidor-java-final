e50880d1c7f7d14fd417120098cfe25a2a624147
Djeison Santos <djeison.adventista@gmail.com>
2026-09-02 12:39:47 -0100

    java1

--- src/main/java/com/labanta/servidorlocal/security/SecurityConfig.java
+++ src/main/java/com/labanta/servidorlocal/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                                 .authorizeHttpRequests(auth -> auth
                                 .requestMatchers( "/api/auth/**").permitAll()
+                                        .requestMatchers("/error").permitAll()
                                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/servicos/**").permitAll()
                                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
